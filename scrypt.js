42587d774023ed767808e35ad9660ce568a72db0
badr554 <badrsalah012@gmail.com>
2024-08-11 20:02:23 +0300

    js problem solving

---
+++ scrypt.js
@@ -0,0 +1,100 @@
+// Question one
+/*
+let x = window.prompt();
+if(x <=100){
+    console.log("number is less than 100") ;
+
+} else{
+    console.log("number is greater than 100") ;
+}
+*/
+
+
+//Question two 
+/* var x = prompt() ; 
+if(x.length>100 ){
+    console.log("String is long");
+}else{
+    console.log("String is short");
+}
+*/
+
+// Question three
+/* let x = prompt();
+let y = "Password"
+if(x = y){
+    console.log("password is valid");
+}else{
+    console.log("not valid");
+}
+*/
+
+//Question four
+/* var x = prompt();
+var y = "saterday"
+if(x = y){
+    console.log("its weekend")
+}
+else {
+    console.log("workday")
+}
+*/
+
+
+//Question 5 
+/*var x = prompt();
+if(x>0){
+console.log("the number is positive")
+}
+else{
+    console.log("number is negative")
+}
+*/
+
+//Question 6 
+/*
+var x = prompt();
+if (x > 80) {
+    console.log("A");
+} else if (x > 70) {
+    console.log("B");
+} else if (x > 60) {
+    console.log("C");
+} else if (x > 50) {
+    console.log("D");
+} else {
+    console.log("F");
+}
+*/
+
+//Question 7
+/*var temp = prompt() ;
+if( temp > 30){
+    console.log("its hot");
+}else{
+    console.log("its cold");
+}
+*/
+
+// Question 8
+/
+var x = prompt(); 
+if(x %2 ==0){
+    console.log("Even Number");
+}else{
+    console.log("Odd Number");
+}
+*/
+
+// Question 9
+/* 
+var x = prompt(); 
+if(x %2 ==0){
+    console.log("Even Number");
+}
+else if(x%2 !=0){
+    console.log("Odd Number");
+}else if (x>20){
+    console.log("you can enter the university")
+}
+*/
